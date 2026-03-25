0f7f0ce4b13f34a17be936b6dda80c63f024bccc
junha park <bd08001@gmail.com>
2026-03-25 19:27:49 +0800

    implement shared code editor

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import * as Y from "yjs";
 import dynamic from "next/dynamic";
 import { editor } from "monaco-editor";
@@ -8,12 +8,13 @@ const Editor = dynamic(() => import("@monaco-editor/react"), { ssr: false });
 
 export default function CodeEditor() {
   const editorRef = useRef<editor.IStandaloneCodeEditor>(null);
+  const [isEditorReady, setEditorReady] = useState(false);
 
-  useEffect(() => {
-    // Dynamically import y-websocket and y-monaco here
-    async function initYjs() {
+  async function initYjs(): Promise<void> {
+    try {
       const { WebsocketProvider } = await import("y-websocket");
       const { MonacoBinding } = await import("y-monaco");
+
       const yDocument = new Y.Doc();
       const provider = new WebsocketProvider(
         `${location.protocol === "http:" ? "ws:" : "wss:"}//localhost:1234`,
@@ -21,7 +22,7 @@ export default function CodeEditor() {
         yDocument,
       );
       // Debug WebSocket connection
-      provider.on("status", (event) => {
+      provider.on("status", (event: { status: string }) => {
         console.log(`WebSocket status: ${event.status}`); // connected/disconnected
       });
 
@@ -38,19 +39,24 @@ export default function CodeEditor() {
       provider.awareness.on("update", () => {
         console.log("Awareness updated", provider.awareness.getStates());
       });
+    } catch (error) {
+      console.error("Error during Yjs initialization:", error);
     }
-
-    if (!editorRef.current) {
-      return;
+  }
+  useEffect(() => {
+    if (isEditorReady) {
+      initYjs();
     }
-    initYjs();
-  }, []);
+    console.log("Yjs initialization triggered");
+  }, [isEditorReady]);
 
-  function handleMount(editor: editor.IStandaloneCodeEditor, Monaco) {
+  function handleMount(editor: editor.IStandaloneCodeEditor, Monaco: any) {
+    console.log("Editor mounted:", editor);
     editorRef.current = editor;
+    setEditorReady(true);
   }
 
-  function handleEditorChange(value, event) {
+  function handleEditorChange(value: string, event: any) {
     console.log("here is the current model value:", value);
   }
 
