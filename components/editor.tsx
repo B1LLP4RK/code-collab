f203968d509c09dd16576b8ff0b09afe67745b66
junha park <bd08001@gmail.com>
2026-03-25 18:24:46 +0800

    setup types

--- components/editor.tsx
+++ components/editor.tsx
@@ -2,53 +2,51 @@
 import { useRef, useEffect } from "react";
 import * as Y from "yjs";
 import dynamic from "next/dynamic";
+import { editor } from "monaco-editor";
 
 const Editor = dynamic(() => import("@monaco-editor/react"), { ssr: false });
 
 export default function CodeEditor() {
-  const editorRef = useRef(null);
+  const editorRef = useRef<editor.IStandaloneCodeEditor>(null);
 
   useEffect(() => {
     // Dynamically import y-websocket and y-monaco here
     async function initYjs() {
-      try {
-        const { WebsocketProvider } = await import("y-websocket");
-        const { MonacoBinding } = await import("y-monaco");
+      const { WebsocketProvider } = await import("y-websocket");
+      const { MonacoBinding } = await import("y-monaco");
+      const yDocument = new Y.Doc();
+      const provider = new WebsocketProvider(
+        `${location.protocol === "http:" ? "ws:" : "wss:"}//localhost:1234`,
+        "monaco",
+        yDocument,
+      );
+      // Debug WebSocket connection
+      provider.on("status", (event) => {
+        console.log(`WebSocket status: ${event.status}`); // connected/disconnected
+      });
 
-        const yDocument = new Y.Doc();
-        const provider = new WebsocketProvider(
-          `${location.protocol === "http:" ? "ws:" : "wss:"}//localhost:1234`,
-          "monaco",
-          yDocument
-        );
+      const type = yDocument.getText("monaco");
 
-        // Debug WebSocket connection
-        provider.on("status", event => {
-          console.log(`WebSocket status: ${event.status}`); // connected/disconnected
-        });
+      const monacoBinding = new MonacoBinding(
+        type,
+        editorRef.current!.getModel()!,
+        new Set([editorRef.current!]),
+        provider.awareness,
+      );
 
-        const type = yDocument.getText("monaco");
-
-        const monacoBinding = new MonacoBinding(
-          type,
-          editorRef.current!.getModel()!,
-          new Set([editorRef.current!]),
-          provider.awareness
-        );
-
-        // Debug Awareness
-        provider.awareness.on("update", () => {
-          console.log("Awareness updated:", provider.awareness.getStates());
-        });
-      } catch (error) {
-        console.error("Failed to initialize Yjs or WebSocketProvider:", error);
-      }
+      // Debug Awareness
+      provider.awareness.on("update", () => {
+        console.log("Awareness updated", provider.awareness.getStates());
+      });
     }
 
+    if (!editorRef.current) {
+      return;
+    }
     initYjs();
   }, []);
 
-  function handleMount(editor, Monaco) {
+  function handleMount(editor: editor.IStandaloneCodeEditor, Monaco) {
     editorRef.current = editor;
   }
 
